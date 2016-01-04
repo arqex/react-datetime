4de50de381545ebca7a183fe82ee1dc424717c81
Edoardo Colombo <edo@podio.com>
2016-01-04 13:34:28 +0100

    Added dev setup.

---
+++ example/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'example.js')
+    ],
+
+    output: {
+        path: path.resolve(__dirname, '.'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    }
+}
