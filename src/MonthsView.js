daa516e60e6f7f7651b94b2ce339f437dc1781be
Vasiliy <vasiliy.p.kostin@gmail.com>
2016-03-01 12:53:04 +0500

    Months selectors is empty if not english locale.
    I tried to use Your datapicker on my page, where moment is swiched to russian location.
    When i click year line, all month choosers was empty. This fix for my problem, maybe its will be helpfull for you.
    
    Thanks for datapicker.

--- src/MonthsView.js
+++ src/MonthsView.js
@@ -54,7 +54,7 @@ var DateTimePickerMonths = React.createClass({
 	},
 
 	renderMonth: function( props, month, year, selectedDate ) {
-		return DOM.td( props, this.props.viewDate.localeData()._monthsShort[ month ] );
+		return DOM.td( props, (moment.monthsShort())[ month ] );
 	}
 });
 
