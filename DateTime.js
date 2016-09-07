02ffc45618ab84b843856879094c7f6946eee910
Light Lin <lilikiki@gmail.com>
2016-09-07 13:14:47 +0800

    Add label prop to have a label text in input
    I just update some code to make it able to have label through prop.

--- DateTime.js
+++ DateTime.js
@@ -28,6 +28,7 @@ var Datetime = React.createClass({
 		onChange: TYPES.func,
 		locale: TYPES.string,
 		input: TYPES.bool,
+		label: TYPES.string,
 		// dateFormat: TYPES.string | TYPES.bool,
 		// timeFormat: TYPES.string | TYPES.bool,
 		inputProps: TYPES.object,
@@ -370,15 +371,42 @@ var Datetime = React.createClass({
 		;
 
 		if ( this.props.input ){
-			children = [ DOM.input( assign({
-				key: 'i',
-				type:'text',
-				className: 'form-control',
-				onFocus: this.openCalendar,
-				onChange: this.onInputChange,
-				onKeyDown: this.onInputKey,
-				value: this.state.inputValue
-			}, this.props.inputProps ))];
+			if ( this.props.label ) {
+				children = [ DOM.div({
+						key: 'd',
+						className: 'form-group'
+					},
+					[ DOM.span({
+							className: 'input-group'
+						},
+						[ DOM.span({
+								key: 'a',
+								className: 'input-group-addon',
+								children: this.props.label
+							}),
+							DOM.input( assign({
+								key: 'i',
+								type:'text',
+								className: 'form-control',
+								onFocus: this.openCalendar,
+								onChange: this.onInputChange,
+								onKeyDown: this.onInputKey,
+								value: this.state.inputValue
+							}, this.props.inputProps ))
+						]
+					)]
+				)]
+			} else {
+				children = [ DOM.input( assign({
+					key: 'i',
+					type:'text',
+					className: 'form-control',
+					onFocus: this.openCalendar,
+					onChange: this.onInputChange,
+					onKeyDown: this.onInputKey,
+					value: this.state.inputValue
+				}, this.props.inputProps ))];
+			}
 		} else {
 			className += ' rdtStatic';
 		}
