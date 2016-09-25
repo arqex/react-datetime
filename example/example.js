f6d467f0b24567d8eb6cbd471977667404328d62
tunessofia <tunes.sofia@gmail.com>
2016-09-25 20:07:49 +0100

    incomplete: add min and max time functionality

--- example/example.js
+++ example/example.js
@@ -1,8 +1,14 @@
 var DateTime = require('../DateTime.js');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var moment = require('moment');
+
+var now =  moment([2015,1,14,15,25,50,125]);
+var timeC = {
+	minTime: now
+}
 
 ReactDOM.render(
-  React.createElement(DateTime, { timeFormat: true }),
+  React.createElement(DateTime, { timeFormat: true, value:now, timeConstraints: timeC }),
   document.getElementById('datetime')
 );
