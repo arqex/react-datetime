95c046589faec5e99f4bf30ae46897fb313195f7
Miloš Rašić <mrasic@gogoair.com>
2016-10-13 13:37:47 +0200

    Fixed the problem with displayed selected datetime not changing when dateFormat and/or timeFromat prop changes. Added example/formatChangeExample.js to demo the change.

--- DateTime.js
+++ DateTime.js
@@ -154,11 +154,8 @@ var Datetime = React.createClass({
 			update = {}
 		;
 
-		if ( nextProps.value !== this.props.value ){
+		if ( nextProps.value !== this.props.value || formats.datetime !== this.getFormats( this.props ).datetime ){
 			update = this.getStateFromProps( nextProps );
-		}
-		if ( formats.datetime !== this.getFormats( this.props ).datetime ) {
-			update.inputFormat = formats.datetime;
 		}
 
 		if ( update.open === undefined ){
