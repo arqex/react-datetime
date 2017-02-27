e094322eda8a05324d6c29cb68ac93f5fb79db6f
Francisco javier Marquez Lopez <javi@JaviBook.local>
2017-02-27 10:41:21 +0100

    Updates react-onclickoutside dependency and creates the Calendar container component

---
+++ src/CalendarContainer.js
@@ -0,0 +1,27 @@
+var React = require('react'),
+  DaysView = require('./DaysView'),
+  MonthsView = require('./MonthsView'),
+  YearsView = require('./YearsView'),
+  TimeView = require('./TimeView'),
+  onClickOutside = require('react-onclickoutside')
+;
+
+var CalendarContainer = onClickOutside( React.createClass({
+	viewComponents: {
+		days: DaysView,
+		months: MonthsView,
+		years: YearsView,
+		time: TimeView
+	},
+
+  render: function(){
+    return React.createElement( this.viewComponents[ this.props.view ], this.props.viewProps );
+  },
+
+  handleClickOutside: function(){
+    this.props.onClickOutside();
+  }
+}));
+
+
+module.exports = CalendarContainer;
