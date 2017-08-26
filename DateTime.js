9509fac6de4707178d9744f2591a1f82790447cd
Jesse Chrestler <JesseChrestler@gmail.com>
2017-08-26 18:58:11 -0400

    if isValidDate is passed we need to verify if the view date is valid … (#296)
    * if isValidDate is passed we need to verify if the view date is valid before trying to render the component.

--- DateTime.js
+++ DateTime.js
@@ -192,7 +192,13 @@ var Datetime = createClass({
 				}
 			}
 		}
-
+		//we should only show a valid date if we are provided a isValidDate function.
+		if (this.props.isValidDate) {
+			updatedState.viewDate = updatedState.viewDate || this.state.viewDate;
+			while (!this.props.isValidDate(updatedState.viewDate)) {
+				updatedState.viewDate = updatedState.viewDate.add(1, 'day');
+			}
+		}
 		this.setState( updatedState );
 	},
 
