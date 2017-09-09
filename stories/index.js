9e75d38e91556072d2e9c3c6b1c9be379033ab01
Martins Giberts <martins.giberts@gmail.com>
2017-09-09 15:38:32 +0200

    Removing examples

--- stories/index.js
+++ stories/index.js
@@ -1,18 +1,52 @@
 import React from 'react'
-
+import moment from 'moment'
 import { storiesOf } from '@storybook/react'
 import withReadme from 'storybook-readme/with-readme'
-import { withInfo } from '@storybook/addon-info'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
+// import { withInfo } from '@storybook/addon-info'
 
 import dateTimeReadMe from '../README.md'
 import DateTime from '../DateTime'
+import Wrapper from './wrapper'
 
 storiesOf('Datetime', module)
+  .addDecorator(withKnobs)
   .addDecorator(withReadme(dateTimeReadMe))
-  .add('main', withInfo('Main Component Preview')(() => (
-    <DateTime
-      viewMode={'months'}
-      dateFormat={'MMMM'}
-      isValidDate={current => current.isBefore(DateTime.moment().startOf('month'))}
-    />
-  )))
+  .add('main', () => (
+    <Wrapper>
+      <DateTime defaultValue={moment()} />
+    </Wrapper>
+  ))
+  .add('Format change preview', () => (
+    <Wrapper>
+      <DateTime
+        dateFormat={text('dateFormat', 'YYYY-MM-DD')}
+        timeFormat={false}
+        defaultValue={moment()}
+      />
+    </Wrapper>
+  ))
+  .add('View change preview', () => (
+    <Wrapper>
+      <DateTime
+        viewMode={select(
+          'viewMode',
+          {
+            years: 'Years',
+            months: 'Months',
+            days: 'Days',
+            time: 'Time',
+          },
+          'days',
+        )}
+        input={false}
+      />
+    </Wrapper>
+  ))
+  //   .add('custom valid date function', () => (
+  //     <DateTime
+  //       viewMode={'months'}
+  //       dateFormat={'MMMM'}
+  //       isValidDate={current => current.isBefore(DateTime.moment().startOf('month'))}
+  //     />
+  //   ))
