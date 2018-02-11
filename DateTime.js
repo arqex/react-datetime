405f4e8754ef801d1837266940bf399c19da74e6
Simon Egersand <s.egersand@gmail.com>
2018-02-11 17:48:56 +0100

    Parse updated viewDate with moment()
    viewDate should always be parsed with moment, in both initial parse and
    updated

--- DateTime.js
+++ DateTime.js
@@ -193,7 +193,7 @@ var Datetime = createClass({
 		}
 
 		if ( nextProps.viewDate !== this.props.viewDate ) {
-			updatedState.viewDate = nextProps.viewDate;
+			updatedState.viewDate = moment(nextProps.viewDate);
 		}
 		//we should only show a valid date if we are provided a isValidDate function. Removed in 2.10.3
 		/*if (this.props.isValidDate) {
