b56db6a87e4c088fe4e64d249d7a819cf3b9f74b
Oleg Kostin <Oleg2tor@gmail.com>
2018-10-04 23:56:35 +0300

    Fix infinity setState when holding multiple buttons down
    Scenario: right mouse down, left mouse down, both mouse up
    Expecting: Time control stops to increase
    Actual: Time control increasing even after both mouse up

--- src/TimeView.js
+++ src/TimeView.js
@@ -170,6 +170,7 @@ var DateTimePickerTime = onClickOutside( createClass({
 			me.setState( update );
 
 			me.timer = setTimeout( function() {
+				clearInterval( me.increaseTimer );
 				me.increaseTimer = setInterval( function() {
 					update[ type ] = me[ action ]( type );
 					me.setState( update );
