5d30170294fcc8b46bc508f3232dff2990a5e74f
Nate Radebaugh <nate.radebaugh@outlook.com>
2019-09-07 13:24:53 -0500

    Remove tests since much functionality has been removed.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  setupFiles: ["jest-date-mock"],
-  roots: ["<rootDir>/test"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
