822f206a15d3ca6f8caae5d502e761045ec46af7
Nate Radebaugh <nradebaugh@bdo.com>
2020-02-01 14:15:28 -0600

    test: Add test to make sure stories/docs don't break.

---
+++ src/DateTime.stories.test.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { render } from "@testing-library/react";
+
+import * as stories from "./DateTime.stories";
+
+test.each(
+  Object.keys(stories).filter(name => typeof stories[name] === "function")
+)("should render %s story", story => {
+  const StoryComponent = stories[story];
+  render(<StoryComponent />);
+});
