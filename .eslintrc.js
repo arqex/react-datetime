565acc1217183764bf73c9e11c10677642f699bd
Nate Radebaugh <nradebaugh@bdo.com>
2021-12-15 15:07:40 -0600

    chore(deps): Update storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     "plugin:react/recommended",
     "prettier",
     "plugin:testing-library/react",
+    "plugin:storybook/recommended",
   ],
   plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
   env: {
@@ -27,43 +28,41 @@ module.exports = {
       version: "16.8.6",
     },
   },
-  globals: { JSX: "readonly", NodeJS: "readonly" },
+  globals: {
+    JSX: "readonly",
+    NodeJS: "readonly",
+  },
   rules: {
     // Help enforce hooks
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-
     // We do not use prop types at the moment
     "react/prop-types": "off",
-
     // Don't enforce "noopener noreferrer" security mitigation for now
     // https://mathiasbynens.github.io/rel-noopener
     "react/jsx-no-target-blank": "off",
-
     // Allow hardcoded " inline
     "react/no-unescaped-entities": "off",
-
     // Catch usages of non-defined (forgotten imports) as errors at compile time
     "no-undef": "error",
-
     // Warn if any var is used (prefer let or const)
     "no-var": "warn",
-
     // Warn if there is an unused variable
-    "no-unused-vars": ["warn", { ignoreRestSiblings: true, args: "none" }],
-
+    "no-unused-vars": [
+      "warn",
+      {
+        ignoreRestSiblings: true,
+        args: "none",
+      },
+    ],
     // Warn if there is a console output
     "no-console": "warn",
-
     // Disallow arrow functions in render functions
     "react/jsx-no-bind": "off",
-
     // Disallow inconsistent returns
     "consistent-return": "error",
-
     // Allow switch/case fallthrough without requiring a "break" statement after each one
     "no-fallthrough": "off",
-
     // Simplify Typescript
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
